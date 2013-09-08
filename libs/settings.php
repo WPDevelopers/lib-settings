6be56849e8dec1978aafd8ba2f09d91a2bc2f67a
Andy Potanin <andy.potanin@usabilitydynamics.com>
2013-09-08 15:30:19 -0400

    WIP

---
+++ libs/settings.php
@@ -0,0 +1,22 @@
+<?php
+  /**
+   * Settings Library
+   *
+   */
+  namespace UsabilityDynamics {
+
+    class Settings extends Utility {
+
+      /**
+       * Settings Class version.
+       *
+       * @public
+       * @static
+       * @property $version
+       * @type {Object}
+       */
+      public static $version = '0.1.1';
+
+    }
+
+  }
