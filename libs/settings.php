eecfe2672d98e9a3bc1be9587c6c9914611cc345
Andy Potanin <andy.potanin@usabilitydynamics.com>
2013-09-08 22:33:22 -0400

    WIP

--- libs/settings.php
+++ libs/settings.php
@@ -15,7 +15,7 @@
        * @property $version
        * @type {Object}
        */
-      public static $version = '0.1.1';
+      public static $version = '0.1.2';
 
     }
 
--- scripts/settings.js
+++ scripts/settings.js
@@ -1,6 +1,7 @@
 /**
  * UD Settings
  *
+ * @version 0.1.2
  * @returns {Object}
  */
 module.exports = function Settings() {
